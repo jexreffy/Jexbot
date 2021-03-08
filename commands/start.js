9d3d2ecd0e9a70a9b8a8c105e926c3e73a3187a9
Jeffrey Bennett <jexreffy@icloud.com>
2021-03-08 11:57:49 -0600

    Gatekeeper start doesn't verify that everyone is ready because that's what the gatekeeper should be doing.

--- commands/start.js
+++ commands/start.js
@@ -4,12 +4,7 @@ const startRace = require('../common/startRace');
 module.exports = (config, race, dChannel, tClient, message, username) => {
     if (message.member && message.member.hasPermission('KICK_MEMBERS', false, false) || config.referees.includes(username)) {
         if (!race.started && race.gatekeeper === username) {
-            let allReady = race.players.every(x => x.ready === true);
-            if (allReady) {
-                startRace(config, race, dChannel);
-            } else {
-                updateRaceMessage(race, dChannel);
-            }
+            startRace(config, race, dChannel);
         }
     }
 }
