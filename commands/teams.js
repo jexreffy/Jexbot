cf7360b4088e04435d0fac80bd0cc44909af50e4
Jeffrey Bennett <jexreffy@icloud.com>
2021-05-16 13:59:45 -0500

    Teams can be reshuffled if desired.

--- commands/teams.js
+++ commands/teams.js
@@ -1,7 +1,7 @@
 const updateRaceMessage = require('../common/updateRaceMessage');
 
 module.exports = (config, db, race, dChannel, username, coop) => {
-    if (!race.started && !race.teams && config.referees.includes(username)) {
+    if (!race.started && config.referees.includes(username)) {
         if (coop && race.players.length % 2 !== 0) {
             dChannel.send(`**${config.teamPlayerError.replace('Teams', 'Co-op')}**`).then().catch(console.error);
             return;
